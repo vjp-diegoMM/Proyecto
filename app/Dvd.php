38c03955de816023570d9d046e1cd562b65389cc
vjp-diegoMM <dmoralesm01@informatica.iesvalledeljerteplasencia.es>
2025-10-21 19:39:11 +0200

    Combinado Proyecto Primera Parte

--- app/Dvd.php
+++ app/Dvd.php
@@ -1,34 +1,4 @@
 <?php
-<<<<<<< HEAD
-    class Dvd extends Soporte
-    {
-        public function __construct($titulo, $numero, $precio, private $idiomas, private $formatoPantalla) {
-            parent::__construct($titulo, $numero, $precio);
-        }
-
-        function setIdiomas($idiomas) {
-            $this->idiomas = $idiomas;
-        }
-
-        function setFormatoPantalla($formatoPantalla) {
-            $this->formatoPantalla = $formatoPantalla;
-        }
-
-        function getIdiomas() {
-            return $this->idiomas;
-        }
-
-        function getFormatoPantalla() {
-            return $this->formatoPantalla;
-        }
-
-        function mostrarResumen() {
-            parent::muestraResumen();
-            echo"Con los idiomas ".$this->idiomas." con un formato de pantalla ".$this->formatoPantalla;
-        }
-    }
-    
-=======
 include_once 'Soporte.php';
 class Dvd extends Soporte
 {
@@ -57,5 +27,4 @@ class Dvd extends Soporte
         echo "Con los idiomas " . $this->idiomas . " con un formato de pantalla " . $this->formatoPantalla;
     }
 }
->>>>>>> Hugo
 ?>
--- app/Juego.php
+++ app/Juego.php
@@ -1,48 +1,4 @@
 <?php
-<<<<<<< HEAD
-    class Juego extends Soporte
-    {
-        public function __construct($titulo, $numero, $precio, private $consola, private $minNumJugadores, private $maxNumJugadores) {
-            parent::__construct($titulo, $numero, $precio);
-        }
-
-        function setConsola($consola) {
-            $this->consola = $consola;
-        }
-
-        function setMinNumJugadores($minNumJugadores) {
-            $this->minNumJugadores = $minNumJugadores;
-        }
-
-        function setMaxNumJugadores($maxNumJugadores) {
-            $this->maxNumJugadores = $maxNumJugadores;
-        }
-
-        function getConsola() {
-            return $this->consola;
-        }
-
-        function getMinNumJugadores() {
-            return $this->minNumJugadores;
-        }
-
-        function getMaxNumJugadores() {
-            return $this->minNumJugadores;
-        }
-
-        function muestraJugadoresPosibles() {
-            echo "<br>Numeros minimos de jugadores es: ".$this->minNumJugadores.", el maximo es: ".$this->maxNumJugadores;
-        }
-
-        function muestraResumen()
-        {
-            parent::muestraResumen();
-            self::muestraJugadoresPosibles();
-            echo "<br>De la consola: ".$this->consola;   
-        }
-    }
-    
-=======
 include_once 'Soporte.php';
 class Juego extends Soporte
 {
@@ -71,5 +27,4 @@ class Juego extends Soporte
         echo"Para la consola ".$this->consola." con un numero de jugadores de ".$this->muestraJugadoresPosibles();
     }
 }
->>>>>>> Hugo
 ?>
